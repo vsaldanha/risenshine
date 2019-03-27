4358101c97c477498f88a51d0c037c18dffda1fc
georgejsp <georgejsp@gmail.com>
2019-03-27 13:00:15 +0530

    Alerts added

--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
+import { CommonModule} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import {LoginModule} from '../login/login.module';
 import {RegisterModule} from '../register/register.module';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 import { DashboardComponent } from './dashboard.component';
-import { LoginComponent } from '../login/login.component';
-import { RegisterComponent } from '../register/register.component';
+import { AlertsComponent } from '../notifications/alerts.component';
+
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
@@ -20,8 +22,10 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
     BsDropdownModule,
     ButtonsModule.forRoot(),
     LoginModule,
-    RegisterModule
+    RegisterModule,
+    CommonModule,
+    AlertModule.forRoot()
   ],
-  declarations: [ DashboardComponent]
+  declarations: [ DashboardComponent, AlertsComponent]
 })
 export class DashboardModule { }
